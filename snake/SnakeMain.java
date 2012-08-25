11415634fb7898cb856998febd5175f950922add
Joshua Liebow-Feeser <joshlf13@gmail.com>
2012-08-25 02:55:40 -0400

    Turned off most debug printing

--- snake/SnakeMain.java
+++ snake/SnakeMain.java
@@ -48,7 +48,7 @@ public class SnakeMain {
 		SnakeMain.snakeCount = snakeCount;
 		snakes = new Snake[snakeCount];
 		for (int i = 0; i < snakeCount; i++) {
-			snakes[i] = new Snake(i, i, 10, i);
+			snakes[i] = new Snake(i, i, 10, 10, i);
 		}
 		
 		FPS = 5;
